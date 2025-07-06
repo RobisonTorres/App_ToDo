2d530eea36cb65205bdcd8bb89b4ee38b1aa8579
RobisonTorres <robison.torres21@gmail.com>
2025-07-06 15:51:09 -0300

    feat: initial commit – organized project components and structure.

---
+++ src/test/java/com/techflow/management/App_ToDo/AppToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techflow.management.App_ToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
