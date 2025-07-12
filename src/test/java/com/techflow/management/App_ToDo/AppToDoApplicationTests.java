394d2cff113307503b82804014bdb522224a99f4
RobisonTorres <robison.torres21@gmail.com>
2025-07-12 15:07:54 -0300

    test: set up integration testing with GitHub Actions
    - Config: configured YAML file for running tests via GitHub Actions.
    - TestClass: created basic test to verify context loads correctly.
    
    refs: #14

--- src/test/java/com/techflow/management/App_ToDo/AppToDoApplicationTests.java
+++ src/test/java/com/techflow/management/App_ToDo/AppToDoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.techflow.management.App_ToDo;
 
+import com.techflow.management.App_ToDo.Controllers.MainController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class AppToDoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MainController controller;
 
+	// This test checks if the Spring application context loads successfully
+	// and if the MainController bean is correctly injected.
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
 }
